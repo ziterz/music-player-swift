fe6d9d8c1fa17a57ec55931b567ebdb5ca92e5d6
Ziterz <ziady.mubaraq@gmail.com>
2024-04-27 13:20:10 +0700

    chore: initial project

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Ziady Mubaraq on 27/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
